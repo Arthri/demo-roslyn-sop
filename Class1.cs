346edf86b3ee46f8b6f4e1171a655da28855e591
Arthri <41360489+a@users.noreply.github.com>
2024-10-05 15:19:25 +0000

    Project files

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System.Runtime.Versioning;
+
+namespace demo_roslyn_sop;
+
+public class Class1
+{
+    [SupportedOSPlatform("windows")]
+    private static void X()
+    {
+        _ = Registry.LocalMachine;
+    }
+
+    private static void Y()
+    {
+        [SupportedOSPlatform("windows")]
+        static void L()
+        {
+            // CA1416: This call site is reachable on all platforms. 'Registry.LocalMachine' is only supported on: 'windows'.
+            _ = Registry.LocalMachine;
+        }
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static void Z()
+    {
+        static void L()
+        {
+            _ = Registry.LocalMachine;
+        }
+    }
+}
